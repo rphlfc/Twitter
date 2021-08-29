f44b9852a68ee60629f5f509005ca8397226c7b5
Raphael Cerqueira <raphaelfc@gmail.com>
2021-08-29 20:05:16 -0300

    Initial Commit

---
+++ Twitter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Twitter
+//
+//  Created by Raphael Cerqueira on 29/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Raphael Cerqueira on 29/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
